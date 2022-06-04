b7dda9332b71158c4229195db2eb11b258744cf3
Jitendra Kumar <76531339+jitendragangwar123@users.noreply.github.com>
2022-06-05 01:41:28 +0530

    initial commit

---
+++ find_average.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.5.0 <0.9.0;
+
+contract day6 {
+    function average(int a,int b,int c) public pure returns(int){
+      require(a>0;b>0;c>0);
+      return (a+b+c)/3;   
+}
+}
