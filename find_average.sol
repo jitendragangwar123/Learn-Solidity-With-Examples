35831353951c44e8a04036a55f77971f785b6c9e
Jitendra Kumar <76531339+jitendragangwar123@users.noreply.github.com>
2022-06-05 01:50:45 +0530

    Update find_average.sol

--- find_average.sol
+++ find_average.sol
@@ -3,7 +3,7 @@ pragma solidity >=0.5.0 <0.9.0;
 
 contract day6 {
     function average(int a,int b,int c) public pure returns(int){
-      require(a>0;b>0;c>0);
+      require(a>0 && b>0 && c>0);
       return (a+b+c)/3;   
 }
 }
