93ac0da231e37ba6e249b1cb4ae9cc4bf4bda1b4
Jitendra Kumar <76531339+jitendragangwar123@users.noreply.github.com>
2022-06-06 01:03:30 +0530

    Create check_prime_number.sol

---
+++ check_prime_number.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.5.0 <0.9.0;
+
+contract Day3 {
+  function prime(uint256 n) public pure returns(uint256){
+    if(n<=1){
+        return 0;
+    }
+    for(uint256 i=2;i<n;i++){
+        if(n%i==0){
+            return 0;
+        }
+    }
+    return 1;  
+    }      
+}
