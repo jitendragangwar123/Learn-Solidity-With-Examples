226247e8eb8a84c66e53ff5fbfa6dd706e60bbb5
Jitendra Kumar <76531339+jitendragangwar123@users.noreply.github.com>
2022-06-07 21:25:54 +0530

    initial commit

---
+++ search_an_element.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.5.0 <0.9.0;
+
+contract Day5 {
+    function search(int256[] memory arr,uint256 len,int256 num)public pure returns(int256){
+    for(uint256 i=0;i<len;i++){
+        if(arr[i]==num){
+            return 1;
+        }
+    }
+    return 0;
+   }  
+}
