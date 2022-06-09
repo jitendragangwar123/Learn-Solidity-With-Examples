4d794723c127fcb7fa381e1ddf0cbf46b6e7a7db
Jitendra Kumar <76531339+jitendragangwar123@users.noreply.github.com>
2022-06-10 01:05:49 +0530

    initial commit

---
+++ books_array.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity >=0.5.0 <0.9.0;
+
+contract Day5 {
+    struct Book{
+        string title;
+        string author;
+        uint256 book_id;
+    }
+    //structure type array
+    Book[10] book;
+    uint256 i=1;
+
+    function setBook(string memory _title,string memory _author,uint256 _id)public{
+        book[i]=Book(_title,_author,_id);
+        i++;
+    }
+    function getBook(uint256 _i)public view returns(string memory,string memory,uint256){
+        return (book[_i].title,book[_i].author,book[_i].book_id);
+    }
+
+}
