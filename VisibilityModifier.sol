b0d2473100790fe784bcf4152924964ef38f3d02
Jitendra Kumar <76531339+jitendragangwar123@users.noreply.github.com>
2022-06-16 16:18:23 +0530

    initial commit

---
+++ VisibilityModifier.sol
@@ -0,0 +1,8 @@
+//SPDX-Licence-Identifier: MIT
+pragma solidity ^0.8.0;
+contract Codiesalert{
+    string public name="Jay"; 
+    string private sex="Male";
+    string internal city="Bareilly";
+
+}
