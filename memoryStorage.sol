54dc3061a4b54e512e27a6313503b7783e59f28b
Jitendra Kumar <76531339+jitendragangwar123@users.noreply.github.com>
2022-06-18 10:46:25 +0530

    initial commit

---
+++ memoryStorage.sol
@@ -0,0 +1,11 @@
+//SPDX-Licence-Identifier: MIT
+pragma solidity ^0.8.0;
+contract Codiesalert{
+    string name="Jay";
+    //memory storage
+    function displayName(string memory _a)public pure returns(string memory){
+        //modify the parameterized variable
+        // _a="gangwar";
+        return _a;
+    }
+}
