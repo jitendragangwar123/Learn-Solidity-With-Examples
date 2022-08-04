4150d3d52c704a6b23db2c33ffd5c58b6ceaf6d5
Jitendra Kumar <76531339+jitendragangwar123@users.noreply.github.com>
2022-08-04 18:54:32 +0530

    initial commit

---
+++ MappingEx.sol
@@ -0,0 +1,13 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+contract MappingEx{
+    struct Employee{
+        string Name;
+        uint Age;
+    }
+    //uint=> ID and Employee => Employees
+    mapping(uint=>Employee) public Employees;
+    function addEmpDetails() public{
+        Employees[1]=Employee("Jay",24);
+    }
+}
