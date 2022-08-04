681c156fc951f17060321ff07c380cb34ab3ba59
Jitendra Kumar <76531339+jitendragangwar123@users.noreply.github.com>
2022-08-04 16:30:22 +0530

    initial commit

---
+++ FUnctionModifiers.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+contract FunctionModifiers{
+    string public name="Jay";
+    address owner;
+    constructor(){
+        owner=msg.sender;
+    }
+    modifier onlyOwner(){
+        require(owner==msg.sender,"You are not authorized person!!!");
+        _;
+    }
+    function changeName(string memory _newName) public onlyOwner{
+        name=_newName;
+    }
+}
