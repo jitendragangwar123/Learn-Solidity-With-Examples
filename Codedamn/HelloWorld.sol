e0f56024d162197a31394bda8eb12cb6c8347e38
Jitendra Kumar <76531339+jitendragangwar123@users.noreply.github.com>
2022-08-28 23:57:59 +0530

    Update HelloWorld.sol

--- Codedamn/HelloWorld.sol
+++ Codedamn/HelloWorld.sol
@@ -1,1 +1,9 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
 
+contract Strings {
+    string res="HelloWorld";
+    function hello() public view returns(string memory){
+        return res;
+    }
+}
