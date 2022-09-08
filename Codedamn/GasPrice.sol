3b0cf3bbaa223975395343c8f7beb078e81eb5a6
Jitendra Kumar <76531339+jitendragangwar123@users.noreply.github.com>
2022-09-08 17:59:32 +0530

    initial commit

--- Codedamn/GasPrice.sol
+++ Codedamn/GasPrice.sol
@@ -4,6 +4,7 @@ pragma solidity >=0.5.0 <0.9.0;
 contract GasPrice {
     function gasPrice() public view returns (uint256) {
     /*Type your statement here in order to get the transaction gas price*/
+    //calculate the gasPrice by using tx.gasprice
         return tx.gasprice;
     }
 }
