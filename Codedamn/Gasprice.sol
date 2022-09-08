9c0d26b543a14f20ca5f3e8026c4c7c3deb34148
Jitendra Kumar <76531339+jitendragangwar123@users.noreply.github.com>
2022-09-08 17:35:07 +0530

    initial commit

---
+++ Codedamn/Gasprice.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.5.0 <0.9.0;
+
+contract GasPrice {
+    function gasPrice() public view returns (uint256) {
+    /*Type your statement here in order to get the transaction gas price*/
+        return tx.gasprice;
+    }
+}
