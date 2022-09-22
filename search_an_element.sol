256f6c31fec7f49119b31e7b2b76fea42f276f29
Jitendra Kumar <76531339+jitendragangwar123@users.noreply.github.com>
2022-09-22 22:18:45 +0530

    Update search_an_element.sol

--- search_an_element.sol
+++ search_an_element.sol
@@ -1,6 +1,5 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.5.0 <0.9.0;
-
 contract Day5 {
     function search(int256[] memory arr,uint256 len,int256 num)public pure returns(int256){
     for(uint256 i=0;i<len;i++){
