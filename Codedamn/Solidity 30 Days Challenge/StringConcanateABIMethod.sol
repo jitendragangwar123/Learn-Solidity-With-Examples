ba8409ab554defe287d77f1303b34cae496b966b
Jitendra Kumar <76531339+jitendragangwar123@users.noreply.github.com>
2022-10-30 23:29:23 +0530

    Initial commit

--- Codedamn/Solidity 30 Days Challenge/StringConcanateABIMethod.sol
+++ Codedamn/Solidity 30 Days Challenge/StringConcanateABIMethod.sol
@@ -1,6 +1,5 @@
 //SPDX-Licence-Identifier: MIT
 pragma solidity ^0.8.0;
-
 contract Codiesalert{
     function StringConcate(string memory _a,string memory _b)public pure returns(string memory){
         return string(abi.encodePacked(_a," ",_b));//string is used for convert the abi code into string 
