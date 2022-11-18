f1e20481f90d5e8b884b6c23901093a739e8e8a8
Jitendra Kumar <76531339+jitendragangwar123@users.noreply.github.com>
2022-11-18 23:50:45 +0530

    initial commit

--- Codedamn/Solidity Programming/ERC20.sol
+++ Codedamn/Solidity Programming/ERC20.sol
@@ -4,6 +4,10 @@ pragma solidity ^0.8.14;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
+/// @title A contract for ERC20 Token
+/// @author Jitendra Kumar
+/// @notice This contract provides the basic details of ERC20 Token like name,symbol,total supply etc.
+
 contract MyToken is ERC20, Ownable {
 
     constructor(address creator) ERC20("META", "3DM") {
