2d9cd7fbe833c323e69a883fdc0528899b729e42
Jitendra Kumar <76531339+jitendragangwar123@users.noreply.github.com>
2022-11-24 23:35:35 +0530

    initial commit

--- Codedamn/Solidity Programming/StateVariable.sol
+++ Codedamn/Solidity Programming/StateVariable.sol
@@ -1,13 +1,12 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.5.0 <0.9.0;
 
-contract StateVar {
-   // declre the state variable rollNumber
-   //Bydefault state variable value depend on datatype 
+contract StateVariable {
+   /*Declare the state variable rollNumber
+   Bydefault state variable value depend on datatype*/
     int public rollNumber; 
     //initialize the value by using constructor
     constructor(){
         rollNumber=10;
     }
-
 }
