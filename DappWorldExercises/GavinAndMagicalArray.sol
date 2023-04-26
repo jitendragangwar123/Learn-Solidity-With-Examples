c9cffa8ab4eef2287d47125dc3eac5b7bb474cc1
Jitendra Kumar <76531339+jitendragangwar123@users.noreply.github.com>
2023-04-26 23:56:22 +0530

    initial commit

--- DappWorldExercises/GavinAndMagicalArray.sol
+++ DappWorldExercises/GavinAndMagicalArray.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.10;
 
 contract Sample {
     function findValue(int[] calldata a,uint ind,uint hrs) public pure returns(int){
-       require(ind < a.length, "Index out of range");
-       return a[ind]*int(hrs);
-		}
+        require(ind < a.length, "Index out of range");
+        return a[ind]*int(hrs);
+	}
  }
