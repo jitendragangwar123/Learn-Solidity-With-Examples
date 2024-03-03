f01c9fccc65ff90a693ad98d3f8e8e187204aa5a
Jitendra Kumar <76531339+jitendragangwar123@users.noreply.github.com>
2024-03-03 23:35:01 +0530

    chore:comments

--- Codedamn/Solidity 30 Days Challenge/reverse_the_number.sol
+++ Codedamn/Solidity 30 Days Challenge/reverse_the_number.sol
@@ -1,15 +1,15 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.5.0 <0.9.0;
-
+// reverse the number
 contract Day6 {
-    function reverseDigit(uint256 n) public pure returns(uint256){
-      uint256 rev=0;
-      uint256  rem;
-      while(n!=0){
-        rem=n%10;
-        rev=rev*10+rem;
-        n=n/10;
-      }
-      return rev;
+    function reverseDigit(uint256 n) public pure returns (uint256) {
+        uint256 rev = 0;
+        uint256 rem;
+        while (n != 0) {
+            rem = n % 10;
+            rev = rev * 10 + rem;
+            n = n / 10;
+        }
+        return rev;
     }
 }
