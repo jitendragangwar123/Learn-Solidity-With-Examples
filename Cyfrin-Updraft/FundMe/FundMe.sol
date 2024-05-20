afdc543fe08735090d88cfbc6c9821a9b544f370
Jitendra Kumar <76531339+jitendragangwar123@users.noreply.github.com>
2024-05-20 17:40:16 +0530

    feat:add funders addresses

--- Cyfrin-Updraft/FundMe/FundMe.sol
+++ Cyfrin-Updraft/FundMe/FundMe.sol
@@ -5,11 +5,17 @@ import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interf
 contract FundMe {
     uint256 public minimumUsd = 5e18;
 
+    address[] public funders;
+    mapping(address funder => uint256 amountFunded) public addressToAmountFunded;
+
     function fund() public payable {
         require(
             getConversionRate(msg.value) >= minimumUsd,
-            "did not send enough eth"
+            "didn't send enough eth"
         ); //1e18=1ETH
+
+        funders.push(msg.sender);
+        addressToAmountFunded[msg.sender]+=msg.value;
     }
 
     // ETH/USD = 0x694AA1769357215DE4FAC081bf1f309aDC325306 (seploia)
