ca13dcb4db9a15e1c18725aad6d60e22995dd10f
Jitendra Kumar <76531339+jitendragangwar123@users.noreply.github.com>
2024-05-21 12:02:28 +0530

    feat:priceconverter library added

---
+++ Cyfrin-Updraft/FundMe/PriceConverter.sol
@@ -0,0 +1,34 @@
+//SPDX-License-Identifier:MIT
+pragma solidity ^0.8.18;
+import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
+
+library PriceConverter {
+    // ETH/USD = 0x694AA1769357215DE4FAC081bf1f309aDC325306 (seploia)
+    function getPrice() internal view returns (uint256) {
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(
+            0x694AA1769357215DE4FAC081bf1f309aDC325306
+        );
+        (, int256 price, , , ) = priceFeed.latestRoundData();
+        //3000_00000000
+        return uint256(price * 1e10);
+    }
+
+    function getConversionRate(uint256 ethAmount)
+        internal
+        view
+        returns (uint256)
+    {
+        uint256 ethPrice = getPrice();
+        // (3000_000000000000000000 * 1_000000000000000000) / 1e18
+        uint256 ethAmountInUsd = (ethPrice * ethAmount) / 1e18;
+        return ethAmountInUsd;
+    }
+
+    function getVersion() internal view returns (uint256) {
+        return AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306).version();
+    }
+}
+
+/*
+    using library you can not initialize thy state variables and can not send ethers.
+*/
