b344051cdefde71010ceef5b7b472e8ce3f35d48
Jitendra Kumar <76531339+jitendragangwar123@users.noreply.github.com>
2024-05-24 15:21:04 +0530

    refactor:modify constructor

--- Cyfrin-Updraft/FundMe/FundMe.sol
+++ Cyfrin-Updraft/FundMe/FundMe.sol
@@ -1,36 +1,58 @@
+/*
+ - transfer
+    payable(msg.sender).transfer(address(this).balance);
+ - send
+    bool success=payable(msg.sender).send(address(this).balance);
+    require(success,"send failed");
+ - call
+    (bool success, ) = payable(msg.sender).call{value: address(this).balance}("");
+    require(success, "call failed");
+
+ - send ether without calling the fund function
+   1. msg.data is not available
+    receive() external payable {
+        fund();
+    }
+
+   2. msg.data is available
+    fallback() external payable {
+        fund();
+    }
+*/
+
 //SPDX-License-Identifier:MIT
 pragma solidity ^0.8.18;
 
+import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
 import {PriceConverter} from "./PriceConverter.sol";
 
-//custom error
-error NotOwner();
-
+error FundMe__NotOwner();
 contract FundMe {
     using PriceConverter for uint256;
     uint256 public constant MINIMUM_USD = 5e18;
     address public immutable i_owner;
 
     address[] public funders;
+    AggregatorV3Interface private s_priceFeed;
     mapping(address => uint256) public addressToAmountFunded;
 
-    constructor() {
+    constructor(address pricefeed) {
         i_owner = msg.sender;
+        s_priceFeed = AggregatorV3Interface(pricefeed);
     }
 
     modifier onlyOwner() {
-        //require(msg.sender==i_owner,"Sender is not owner!");
         if (msg.sender != i_owner) {
-            revert NotOwner();
+            revert FundMe__NotOwner();
         }
         _;
     }
 
     function fund() public payable {
         require(
-            msg.value.getConversionRate() >= MINIMUM_USD,
-            "did not send enough eth"
-        ); //1e18=1ETH
+            msg.value.getConversionRate(s_priceFeed) >= MINIMUM_USD,
+            "didn't send enough eth"
+        );
 
         funders.push(msg.sender);
         addressToAmountFunded[msg.sender] += msg.value;
@@ -40,30 +62,24 @@ contract FundMe {
         for (uint256 i = 0; i < funders.length; i++) {
             addressToAmountFunded[funders[i]] = 0;
         }
-        //reset the funders array
+
         funders = new address[](0);
-
-        //transfer
-        // payable(msg.sender).transfer(address(this).balance);
-
-        //send
-        // bool success=payable(msg.sender).send(address(this).balance);
-        // require(success,"send failed");
-
-        //call
         (bool success, ) = payable(msg.sender).call{
             value: address(this).balance
         }("");
         require(success, "call failed");
     }
 
-    // send ether without calling the fund function
-    //msg.data is not available
+    function getVersion() public view returns (uint256) {
+        return s_priceFeed.version();
+    }
+
     receive() external payable {
         fund();
     }
-    //msg.data is available
+
     fallback() external payable {
         fund();
     }
 }
+
