43c5532772f278a9fc841996de5b731f77dc4f1f
Jitendra Kumar <76531339+jitendragangwar123@users.noreply.github.com>
2024-05-24 15:16:10 +0530

    refactor:pricefeed address

--- Cyfrin-Updraft/FundMe/PriceConverter.sol
+++ Cyfrin-Updraft/FundMe/PriceConverter.sol
@@ -1,34 +1,28 @@
+/*
+library:
+   In library you can not initialize the state variables and also you can not send ethers.
+   ETH/USD = 0x694AA1769357215DE4FAC081bf1f309aDC325306 (seploia)
+*/
+
 //SPDX-License-Identifier:MIT
 pragma solidity ^0.8.18;
 import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
 
 library PriceConverter {
-    // ETH/USD = 0x694AA1769357215DE4FAC081bf1f309aDC325306 (seploia)
-    function getPrice() internal view returns (uint256) {
-        AggregatorV3Interface priceFeed = AggregatorV3Interface(
-            0x694AA1769357215DE4FAC081bf1f309aDC325306
-        );
+    function getPrice(AggregatorV3Interface priceFeed) internal view returns (uint256) {    
         (, int256 price, , , ) = priceFeed.latestRoundData();
         //3000_00000000
         return uint256(price * 1e10);
     }
 
-    function getConversionRate(uint256 ethAmount)
+    function getConversionRate(uint256 ethAmount, AggregatorV3Interface priceFeed)
         internal
         view
         returns (uint256)
     {
-        uint256 ethPrice = getPrice();
+        uint256 ethPrice = getPrice(priceFeed);
         // (3000_000000000000000000 * 1_000000000000000000) / 1e18
         uint256 ethAmountInUsd = (ethPrice * ethAmount) / 1e18;
         return ethAmountInUsd;
     }
-
-    function getVersion() internal view returns (uint256) {
-        return AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306).version();
-    }
 }
-
-/*
-    using library you can not initialize thy state variables and can not send ethers.
-*/
