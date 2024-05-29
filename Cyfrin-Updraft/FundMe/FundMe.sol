3e81f0b3d677024c66d005d83826fc81748565ed
Jitendra Kumar <76531339+jitendragangwar123@users.noreply.github.com>
2024-05-29 11:44:48 +0530

    refactor:calling functions order

--- Cyfrin-Updraft/FundMe/FundMe.sol
+++ Cyfrin-Updraft/FundMe/FundMe.sol
@@ -20,6 +20,18 @@
     }
 */
 
+/*
+Functions Order:
+ 1.constructor
+ 2.receive
+ 3.fallback
+ 4.external
+ 5.public
+ 6.internal
+ 7.private
+ 8.view / pure
+*/
+
 //SPDX-License-Identifier:MIT
 pragma solidity ^0.8.18;
 
@@ -35,12 +47,9 @@ contract FundMe {
     address[] public funders;
     AggregatorV3Interface private s_priceFeed;
     mapping(address => uint256) public addressToAmountFunded;
+    // events 
 
-    constructor(address pricefeed) {
-        i_owner = msg.sender;
-        s_priceFeed = AggregatorV3Interface(pricefeed);
-    }
-
+    // modifier
     modifier onlyOwner() {
         if (msg.sender != i_owner) {
             revert FundMe__NotOwner();
@@ -48,6 +57,22 @@ contract FundMe {
         _;
     }
 
+    //constructor
+    constructor(address pricefeed) {
+        i_owner = msg.sender;
+        s_priceFeed = AggregatorV3Interface(pricefeed);
+    }
+
+    //receive
+    receive() external payable {
+        fund();
+    }
+
+    //fallback
+    fallback() external payable {
+        fund();
+    }
+    // public function
     function fund() public payable {
         require(
             msg.value.getConversionRate(s_priceFeed) >= MINIMUM_USD,
@@ -57,7 +82,7 @@ contract FundMe {
         funders.push(msg.sender);
         addressToAmountFunded[msg.sender] += msg.value;
     }
-
+    // public function
     function withdraw() public onlyOwner {
         for (uint256 i = 0; i < funders.length; i++) {
             addressToAmountFunded[funders[i]] = 0;
@@ -69,17 +94,12 @@ contract FundMe {
         }("");
         require(success, "call failed");
     }
+    // internal function
 
+    // private function
+
+    // view function
     function getVersion() public view returns (uint256) {
         return s_priceFeed.version();
     }
-
-    receive() external payable {
-        fund();
-    }
-
-    fallback() external payable {
-        fund();
-    }
 }
-
