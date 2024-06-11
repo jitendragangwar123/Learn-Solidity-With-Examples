78681f54bdb7ad843cd5815a8f40f1824557e1ef
Jitendra Kumar <76531339+jitendragangwar123@users.noreply.github.com>
2024-06-11 23:46:47 +0530

    feat:Lottery.sol file added

--- Cyfrin-Updraft/Lottery-Game/Lottery.sol
+++ Cyfrin-Updraft/Lottery-Game/Lottery.sol
@@ -1,6 +1,47 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
 
+/**
+ * @title A sample Raffle contract
+ * @author Jay Gangwar
+ * @notice This contract is for creating a sample raffle
+ * @dev Implements Chainlink VRFv2
+ */
+
 contract Lottery {
-    uint256 private s_entranceFee;
+    //custom error
+    error Lottery_NotEnoughEthSent();
+
+    uint256 private immutable i_entranceFee;
+   //  @dev duration of the lottery in seconds
+    uint256 private immutable i_interval;
+    address payable[] private s_players;
+    uint256 private s_lastTimeStamp;
+
+    //events
+    event EnterRaffle(address indexed player);
+
+    constructor(uint256 entranceFee, uint256 interval) {
+        i_entranceFee = entranceFee;
+        i_interval = interval;
+        s_lastTimeStamp=block.timestamp;
+    }
+
+    function enterRaffle() external payable {
+        if (msg.value < i_entranceFee) {
+            revert Lottery_NotEnoughEthSent();
+        }
+        s_players.push(payable(msg.sender));
+        emit EnterRaffle(msg.sender);
+    }
+
+    function pickWinner() external {
+        if((block.timestamp-s_lastTimeStamp) < i_interval){
+            revert();
+        }
+    }
+
+    function getEntranceFee() external view returns (uint256) {
+        return i_entranceFee;
+    }
 }
