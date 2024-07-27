5d0868bc3fc1d0465b72160fceccbbb04fcf17cd
Jitendra Kumar <76531339+jitendragangwar123@users.noreply.github.com>
2024-07-27 22:58:17 +0530

    chore: comments

--- Cyfrin-Updraft/StorageFactory.sol
+++ Cyfrin-Updraft/StorageFactory.sol
@@ -4,13 +4,14 @@ pragma solidity 0.8.18;
 import {SimpleStorage} from "./SimpleStorage.sol";
 
 contract StorageFactory {
+    // store the contract addresses 
     SimpleStorage[] public listOfSimpleStorageContracts;
-
+    // create the smart contract
     function createSimpleStorageContract() public {
         SimpleStorage newSimpleStorageContract = new SimpleStorage();
         listOfSimpleStorageContracts.push(newSimpleStorageContract);
     }
-
+    // add new person details
     function sfAddPerson(
         uint256 _simpleStorageIndex,
         uint256 _newNumber,
@@ -21,7 +22,7 @@ contract StorageFactory {
             _newName
         );
     }
-
+    // get person details 
     function getPersonDetails(uint256 _simpleStorageIndex)
         public
         view
