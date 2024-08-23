c3916149906f419469e94a9b15a9e21e2d6e21db
Jitendra Kumar <76531339+jitendragangwar123@users.noreply.github.com>
2024-08-23 23:48:55 +0530

    chore: comments

--- Cyfrin-Updraft/Lottery-Game/Lottery.sol
+++ Cyfrin-Updraft/Lottery-Game/Lottery.sol
@@ -20,14 +20,13 @@ contract Lottery {
 
     //events
     event EnterRaffle(address indexed player);
-
     constructor(uint256 entranceFee, uint256 interval) {
         i_entranceFee = entranceFee;
         i_interval = interval;
         s_lastTimeStamp=block.timestamp;
     }
 
-    // function to enter the game
+    //function to enter the game
     function enterRaffle() external payable {
         if (msg.value < i_entranceFee) {
             revert Lottery_NotEnoughEthSent();
@@ -35,13 +34,15 @@ contract Lottery {
         s_players.push(payable(msg.sender));
         emit EnterRaffle(msg.sender);
     }
-   // function to pick the winner
+
+   //function to pick the winner
     function pickWinner() external {
         if((block.timestamp-s_lastTimeStamp) < i_interval){
             revert();
         }
     }
-   // function to get entrance fee
+
+   //function to get entrance fee
     function getEntranceFee() external view returns (uint256) {
         return i_entranceFee;
     }
